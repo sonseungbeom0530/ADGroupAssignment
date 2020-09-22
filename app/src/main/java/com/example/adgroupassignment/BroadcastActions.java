16937fa8c98a500a3e4dec01565bf869cabb4400
sonseungbeom0530 <ssb05300@naver.com>
2020-09-22 23:48:02 +0800

    fix error

--- app/src/main/java/com/example/adgroupassignment/BroadcastActions.java
+++
@@ -1,6 +0,0 @@
-package com.example.adgroupassignment;
-
-public class BroadcastActions {
-    public final static String PREPARED = "PREPARED";
-    public final static String PLAY_STATE_CHANGED = "PLAY_STATE_CHANGED";
-}
